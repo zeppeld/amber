c4dc209e4e2b6b394847a77a745c549114657569
Nischal77 <nischalchand48@gmail.com>
2020-07-20 17:14:40 +0545

    changes made and new files added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:amber/ambulance/ambulancelist.dart';
 import 'package:amber/auth_screens/login.dart';
+import 'package:amber/auth_screens/wrapper.dart';
+import 'package:amber/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:amber/auth_screens/welcome.dart';
+import 'package:provider/provider.dart';
+import 'package:amber/model/user.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,13 +14,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        routes: {
-          "Welcome": (context) => Welcome(),
-          "Login": (context) => Login(),
-          "AmbulanceList": (context) => AList()
-        },
-        home: Welcome());
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          routes: {
+            "Welcome": (context) => Welcome(),
+            "Login": (context) => Login(),
+            "AmbulanceList": (context) => AList()
+          },
+          home: Wrapper()),
+    );
   }
 }
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,36 +1,50 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:amber/model/user.dart';
 
 class AuthService {
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Stream<String> get onAuthStateChanged => _firebaseAuth.onAuthStateChanged.map(
-        (FirebaseUser user) => user?.uid,
-      );
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  Stream<User> get user {
+    return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
+  }
 
   //Email & Password Sign Up
-  Future<String> createUserWithEmailAndPassword(
-      String email, String password, String name) async {
-    final currentUser = await _firebaseAuth.createUserWithEmailAndPassword(
-        email: email, password: password);
-
-    // Update the username
-    var userUpdateInfo = UserUpdateInfo();
-    UserUpdateInfo.displayName = name;
-    await currentUser.updateProfile(UserUpdateInfo);
-    await currentUser.reload();
-    return currentUser.uid;
+  Future createUserWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
   }
 
 //Email & Password Sign In
-  Future<String> signInWithEmailAndPassword(
-      String email, String password) async {
-    return (await _firebaseAuth.signInWithEmailAndPassword(
-            email: email, password: password))
-        .uid;
+  Future<User> signInWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
   }
 
   //Sign Out
-  signOut() {
-    return _firebaseAuth.signOut();
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
   }
 }
